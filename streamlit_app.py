008bd1970d6fc10908816074adb1da98945229bf
Daryl Lim <73304404+darylalim@users.noreply.github.com>
2026-01-09 07:00:02 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,6 +118,6 @@ if uploaded_file is not None:
         st.download_button(
             label="Download CSV",
             data=csv_data,
-            file_name=f"{uploaded_file.name}_sentiment.csv",
+            file_name=f"{uploaded_file.name.rsplit('.', 1)[0]}_sentiment.csv",
             mime="text/csv"
         )
